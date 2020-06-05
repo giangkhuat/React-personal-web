1cd045eb28e557a1e4724cfc16649ca8bf35cf2a
giangkhuat <khuatgia@grinnell.edu>
2020-06-04 22:17:42 -0500

    fix projects folder

--- src/components/contents/Social.js
+++ src/components/contents/Social.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 function Social() {
   return (
     <div class="social">
+      <a href="https://www.linkedin.com/in/giangkhuat/" target="_blank">
+        <i class="fa fa-envelope-square"></i>
+      </a>
       <a href="https://github.com/giangkhuat" target="_blank">
         <i class="fa fa-github"></i>
       </a>
